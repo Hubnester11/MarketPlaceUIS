269afb1c49d41b94cd0a3f17d59ae5e63950f84a
Hubnester11 <juancamilopertuz69@gmail.com>
2023-02-10 12:47:22 -0500

    Main

--- Main.py
+++ Main.py
@@ -1,2 +1,15 @@
-#pip install fastApi pip install "uvicorn[standard]" //Instalacion de fastapi y uvicorn
+#pip install fastApi 
+#pip install "uvicorn[standard]"  #Instalacion de fastapi y uvicorn
+from fastapi import FastAPI
+from typing import Union
+from pydantic import BaseModel
+from conexion_postgresql import *
 
+
+app = FastAPI()
+
+@app.get('/')
+def read_root():
+    return {'Hello': 'World!!!'}
+
+    
